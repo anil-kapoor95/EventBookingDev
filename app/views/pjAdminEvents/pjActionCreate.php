782e2bd9b1016a85b69e6346d5f9c9ef07be6132
Dev <anilchd.kapoor93@gmail.com>
2026-08-05 10:23:25 +0530

    added max ticket in create event

--- app/views/pjAdminEvents/pjActionCreate.php
+++ app/views/pjAdminEvents/pjActionCreate.php
@@ -238,6 +238,7 @@ $short_days = __('short_days', true);
                                             	<th><?php __('lblType');?></th>
                                                 <th><?php __('lblPrice');?></th>
                                                 <th><?php __('lblAvailable');?></th>
+                                                <th><?php __('lblMaxPurchase');?></th>
                                                 <th></th>
                                             </tr>
                                         </thead>
@@ -284,7 +285,13 @@ $short_days = __('short_days', true);
 																<input class="touchspin3 required" type="text" name="available[<?php echo $index;?>]" id="available_<?php echo $index;?>" value="<?php echo $price['available']; ?>" data-msg-required="<?php __('ebc_field_required');?>" />
 															</div>
 														</td>
-														
+
+														<td>
+															<div class="form-group">
+																<input class="touchspin3" type="text" name="max_purchase[<?php echo $index;?>]" id="max_purchase_<?php echo $index;?>" value="<?php echo isset($price['max_purchase']) ? (int) $price['max_purchase'] : 0; ?>" />
+															</div>
+														</td>
+
 														<td>
 															<?php if($k > 0) { ?>
 																<div class="m-t-xs text-right">
@@ -334,7 +341,13 @@ $short_days = __('short_days', true);
 															<input class="touchspin3 required" value="5" type="text" name="available[<?php echo $index;?>]" id="available_<?php echo $index;?>" data-msg-required="<?php __('ebc_field_required');?>" />
 														</div>
 													</td>
-													
+
+													<td>
+														<div class="form-group">
+															<input class="touchspin3" value="0" type="text" name="max_purchase[<?php echo $index;?>]" id="max_purchase_<?php echo $index;?>" />
+														</div>
+													</td>
+
 	                                                <td>
 	                                                    &nbsp;
 	                                                </td>
